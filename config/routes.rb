3d90e10f5bcc2c5ba2f184b154da149811e7ffec
Anum Qudsia <anumqudsia@hotmail.com>
2013-02-17 03:15:20 +0000

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Lc::Application.routes.draw do
+  root :to => "pages#home"
+  
+  get "pages/home"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
