9dd72886d26dd9337d4d91f446dd5a4538ffadde
Anum Qudsia <anumqudsia@hotmail.com>
2013-02-19 04:16:01 +0000

    products working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create(name: 'Blue Jeans', description: 'Good quality jeans', price: '29', photo: 'img02.png' )
+#Product.create(name: 'Blue Jeans', description: 'Good quality jeans', price: '29', photo: 'img02.png' )
